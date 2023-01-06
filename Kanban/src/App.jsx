0a14a4e37e748e3d6be1e11c53441affce11b589
Marcos Almeida Koch <115181147+Marcos0098@users.noreply.github.com>
2023-01-06 18:56:49 -0300

    drag and drop
    criado a função de drag and drop dentro do to do com a biblioteca react-beautiful-dnd

--- Kanban/src/App.jsx
+++ Kanban/src/App.jsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
+<<<<<<< HEAD
+import { useState } from 'react';
+import Modal from 'react-modal';
+=======
+import { useEffect, useState } from 'react'
 import Modal from 'react-modal'
+>>>>>>> parent of dac6a63 (botão de edit)
 
-import {BsTrash} from 'react-icons/bs'
-import {BiEditAlt} from 'react-icons/bi'
+import {BsTrash} from 'react-icons/bs';
+import {BiEditAlt} from 'react-icons/bi';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import './App.css'
 
@@ -21,37 +27,39 @@ Modal.setAppElement(document.getElementById('root'));
 
 function App() {
   const [modalOpen, setModalOpen] = useState();
-  const [modalEditOpen, setModalEditOpen] = useState();
-
   const [id, setID] = useState(0);
-
   const [task, setTask] = useState();
+<<<<<<< HEAD
   const [editedTask, setEditedTask] = useState();
   const [editedIndex, setEditedIndex] = useState();
+  const [listaTodo, setListaTodo] = useState([])
+  const [listaDoing, setListaDoing] = useState([{taskDesc:"asdasdas", taskID:20}])
+  // open e close Modal para inserir uma task
+=======
   const [listaTask, setListaTask] = useState([])
 
-  // open e close Modal para inserir uma task
+>>>>>>> parent of dac6a63 (botão de edit)
   function openModal() {
     setModalOpen(true);
   }
 
   function closeModal() {
     setModalOpen(false);
+<<<<<<< HEAD
   }
-  // open e close Modal para inserir uma task
+=======
+>>>>>>> parent of dac6a63 (botão de edit)
 
-  // open e close Modal para editar uma task
-  function openModalEdit(index) {
-    setModalEditOpen(true);
-    setEditedIndex(index);
   }
 
+<<<<<<< HEAD
   function closeModalEdit() {
     setModalEditOpen(false);
   }
-  // open e close Modal para inserir uma task
 
   //Submit para adicionar a task na lista "TO DO"
+=======
+>>>>>>> parent of dac6a63 (botão de edit)
   const handleTask = (e) => {
     e.preventDefault();
     const value = e.target.value;
@@ -59,22 +67,23 @@ function App() {
     setTask(value)
   }
 
-  
-  const handleSubmit = () =>{
+  const handleSubmit = (e) =>{
+    e.preventDefault();
     
-    const newTask = [...listaTask, {
+    const newTask = [...listaTodo, {
       taskDesc: task,
       taskID: id,
       },
     ];
 
-    setListaTask(newTask);
+    setListaTodo(newTask);
     setTask("");
     setID(id + 1);
     closeModal();
-  }
-  //Submit para adicionar a task na lista "TO DO"
 
+  }
+
+<<<<<<< HEAD
   //Submit para Editar a task
   const handleEditedTask = (e) => {
     e.preventDefault();
@@ -84,48 +93,95 @@ function App() {
   }
   
   const handleEdit = () => {
-    const listaCopy = Array.from(listaTask);
+    const listaCopy = Array.from(listaTodo);
     listaCopy.splice(editedIndex, 1, {taskDesc: editedTask});
 
 
-    setListaTask(listaCopy);
+    setListaTodo(listaCopy);
     setEditedTask("");
     closeModalEdit();
   }
-  //Submit para Editar a task
 
   //function para deletar Task
   const deletar = (index) => {
+    const listaCopy = Array.from(listaTodo);
+=======
+  const deletar = (index) => {
+    console.log(`o id para deletar é: ${index}`);
     const listaCopy = Array.from(listaTask);
+>>>>>>> parent of dac6a63 (botão de edit)
     listaCopy.splice(index, 1)
-    setListaTask(listaCopy)
+    setListaTodo(listaCopy)
   }
-  //function para deletar Task
 
+<<<<<<< HEAD
+  //Alterar a lista de tasks ao arrastar e soltar
+  function handleOnDragEnd(result) {
+    if (!result.destination) return;
+    const items = Array.from(listaTodo);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
+
+    setListaTodo(items);
+  }
+
+
+  
   //Logs de teste
+  console.log(listaDoing)
+  //Logs de teste
+
+=======
+
+  console.log(listaTask)
   console.log("Quantidade de IDs ja criados é de :", id)
-  //Logs de teste
-
+>>>>>>> parent of dac6a63 (botão de edit)
   return (
     <div className="App">
       <div className="container-geral">
-
+        <DragDropContext onDragEnd={handleOnDragEnd}>
         <div className="container-task">
           <div className="todo">
             <h2>To do</h2>
+<<<<<<< HEAD
+            
+              <Droppable droppableId="desafios-todo">
+              {(provided) => (
+                  <ul className="desafios-todo" {...provided.droppableProps} ref={provided.innerRef}>
+                  {listaTodo.map((desafios,index) => {
+                    return(
+                      <Draggable key={desafios.taskDesc} draggableId={desafios.taskDesc} index={index}>
+                        {(provided) => (
+                          <li className='miniContainer-task' key={desafios.taskDesc} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <p>{desafios.taskDesc}</p>
+                            <div className="icons-task">
+                              <button onClick={() => openModalEdit(index)}><BiEditAlt/></button>
+                              <button onClick={() => deletar()}><BsTrash/></button>
+                            </div>
+                          </li>
+                        )}
+                      </Draggable>
+                    ) 
+                  })}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            
+=======
             <div className="tasks-todo">
               {listaTask.taskDesc != "" && listaTask.map((desafios,index) => {
                 return(<div className='miniContainer-task'>
                   <p>{desafios.taskDesc}</p>
                   <div className="icons-task">
-                    <button onClick={() => openModalEdit(index)}><BiEditAlt/></button>
-                    <button onClick={() => deletar()}><BsTrash/></button>
+                    <button ><BiEditAlt/></button>
+                    <button onClick={() => deletar(index)}><BsTrash/></button>
                   </div>
                 </div>
                 ) 
               })}
+>>>>>>> parent of dac6a63 (botão de edit)
 
-            </div>
             <button className='adicionar-task' onClick={openModal}>Adicionar uma task</button>
             <div className='Modal'>
               <Modal
@@ -140,32 +196,44 @@ function App() {
                   <input type="submit" value="Salvar" />
                   <button onClick={closeModal}>Close</button>
                 </form>
-              </Modal>
 
 
-              <Modal
-                isOpen={modalEditOpen}
-                onRequestClose={closeModalEdit}
-                style={customStyles}
-                contentLabel="Edit Task"
-              >
-                <h2>Edite a task abaixo</h2>
-                <form onSubmit={handleEdit}>
-                  <input type="text" placeholder='Escreva aqui sua nova tarefa!' value={editedTask} onChange={handleEditedTask}/>
-                  <input type="submit" value="Salvar" />
-                  <button onClick={closeModalEdit}>Close</button>
-                </form>
               </Modal>
             </div>
+           
           </div>
+
         </div>
 
         <div className="container-task">
           <div className="doing">
             <h2>Doing</h2>
-            
+<<<<<<< HEAD
+              <Droppable droppableId="desafios-doing">
+              {(provided) => (
+                <ul className='desafios-doing' {...provided.droppableProps} ref={provided.innerRef}>
+                  {listaDoing.map((desafios, index) => {
+                    return(
+                      <Draggable key={desafios.taskDesc} draggableId={desafios.taskDesc} index={index}>
+                        {(provided) => (
+                          <li className='miniContainer-task' key={desafios.taskDesc}  ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+                            <p>{desafios.taskDesc}</p>
+                            <div className="icons-task">
+                              <button onClick={() => openModalEdit(index)}><BiEditAlt/></button>
+                              <button onClick={() => deletar()}><BsTrash/></button>
+                            </div>
+                          </li>
+                        )}
+                      </Draggable>
+                    )
+                  })}
+                    {provided.placeholder}
+                </ul>
+                )}
+              </Droppable>
+=======
+>>>>>>> parent of dac6a63 (botão de edit)
           </div>
-
         </div>
 
         <div className="container-task">
@@ -174,8 +242,9 @@ function App() {
           </div>
 
         </div>
-
+        </DragDropContext> 
       </div>
+
     </div>
   )
 }
